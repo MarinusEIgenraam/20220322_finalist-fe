db77a8304faa852d030784bcf2a6d1dd8ea7d2db
MarinusEIgenraam <rinuseigenraam@gmail.com>
2022-03-27 07:34:50 +0200

    Basic setup
    - installed necessary dependencies

---
+++ src/components/ArcDiagram.js
@@ -0,0 +1,24 @@
+////////////////////
+//// Build
+import React, { useState } from 'react'
+import styled from 'styled-components';
+
+////////////////////
+//// Environmental
+
+////////////////////
+//// External
+
+export default function ArcDiagram({ data, dimensions }) {
+
+    return (
+        <>
+        </>
+    )
+}
+
+const NewArcDiagram = styled.div`
+
+`
+
+/** Created by ownwindows on 27-03-22 **/
